35b14f9aeea8cd0cc252f0db510986967becc3b6
vahanafk <vahan.ghazaryan.00@mail.ru>
2023-10-21 12:35:18 +0400

    minimum viable get method

--- Server.hpp
+++ Server.hpp
@@ -1,5 +1,24 @@
 #pragma once
-// #include "Client.hpp"
+#include "Client.hpp"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <signal.h>
+#include <errno.h>
+
+
+#include <iostream>
+#include <fstream>
+
+const int _clientLimit = 1000;
+const int _serverLimit = 1000;
 
 class Server
 {
@@ -9,7 +28,8 @@ class Server
         ~Server() {};
         bool start() {
             int a = 1;
-            _serverSockets[0] = socket (AF_INET, SOCK_STREAM, 0);
+            _serverSockets[0] = socket(AF_INET, SOCK_STREAM, 0);
+
             if (setsockopt(_serverSockets[0], SOL_SOCKET, SO_REUSEADDR, &a, sizeof(int)) < 0) {
                 std::cerr << "setsockopt(SO_REUSEADDR) failed" << std::endl;
             }
@@ -17,31 +37,92 @@ class Server
             struct sockaddr_in addr;
             memset(&addr , 0 , sizeof(struct sockaddr_in));
             addr.sin_family = AF_INET;
-            addr.sin_addr.s_addr = inet_addr(_ipAddress);
+            addr.sin_addr.s_addr = inet_addr(_ipAddress.c_str());
             addr.sin_port = htons(_port);
             std::cout << "addr.sin_port = " << addr.sin_port << std::endl;
             std::cout << "addr.sin_addr.s_addr = " << addr.sin_addr.s_addr << std::endl;
+
             if (bind(_serverSockets[0], (sockaddr *)&addr, sizeof(addr)) == -1) {
                 perror("bind: ");
+                exit(1);
             }
-            // signal(SIGINT, INThandler);
+
             if (listen(_serverSockets[0], 0) == -1) {
                 perror("listen: ");
+                exit(1);
             };
+
+            while(true) {
+                // struct sockaddr_in addrClient;
+                // socklen_t len;
+                // memset(&addrClient , 0 , sizeof(struct sockaddr_in));
+                // clientSocket = accept(serverSocket, (sockaddr *)&addrClient, &len);
+                int clientSocket = accept(_serverSockets[0], NULL, NULL);
+                // std::cout << "addrClient.sin_port = " << addrClient.sin_port << std::endl;
+                // std::cout << "addrClient.sin_addr.s_addr = " << addrClient.sin_addr.s_addr << std::endl;
+
+                if (clientSocket == -1) {
+                    perror("accept: ");
+                    exit(1);
+                }
+                char buf[2044];
+
+                if (recv(clientSocket, buf, sizeof(buf), 0) == -1) {
+                    perror("recv :");
+                    exit(1);
+                }
+                std::cout << buf << std::endl;
+                const char *response = get("./ind.html");
+                if (send(clientSocket, response, strlen(response), 0) == -1)
+                {
+                    perror("send :");
+                    exit(1);
+                }
+                close(clientSocket);
+            }
         };
-        bool accept() {
 
-            _clients new(accept(serverSocket, (sockaddr *)&addrClient, &len));
-        }
+        const char *get(std::string request) {
+            std::string response;
+            int statusCode = 200;
+            std::string status = "OK\r\n";
+            std::string contentType = "Content-Type: text/html\r\n";
 
-        char *get();
+            response += "HTTP/1.1 ";
+
+            if (access(request.c_str(), F_OK) == 0) {   // TODO check permission to read
+                std::string fileContent;
+                std::ifstream ifs(request);
+                if (ifs.is_open() == false) {
+                    throw std::logic_error("can not open file");
+                }
+                std::getline(ifs, fileContent, '\0');
+                response += std::to_string(statusCode);
+                response +=  status;
+                response +=  contentType;
+                response +=  "Content-Length: " + std::to_string(fileContent.size());
+                response +=  "\n\n";
+                response +=  fileContent;
+            } else {
+                std::string fileContent;
+                std::ifstream ifs("./error_pages/404.html");
+                if (ifs.is_open() == false) {
+                    throw std::logic_error("can not open file");
+                }
+                std::getline(ifs, fileContent, '\0');
+                response += std::to_string(404);
+                response +=   "not found";
+                response +=  contentType;
+                response +=  "Content-Length: " + std::to_string(fileContent.size());
+                response +=  "\n\n";
+                response +=  fileContent;
+            }
+            return (response.c_str());
+        };
         bool post();
         bool del();
     private:
-        const int _clientLimit = 1000;
-        const int _serverLimit = 1000;
         std::string _ipAddress;
         int _port;
         int _serverSockets[_serverLimit];
-        std::vector<Client> _clients;
 };
--- main.cpp
+++ main.cpp
@@ -7,67 +7,76 @@
 #include<signal.h>
 #include <errno.h>
 
+#include "Server.hpp"
+
 int main() {
-    int a = 1;
-    int serverSocket = socket (AF_INET, SOCK_STREAM, 0);
-    if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &a, sizeof(int)) < 0) {
-        std::cerr << "setsockopt(SO_REUSEADDR) failed" << std::endl;
-    }
-    struct sockaddr_in addr;
-    memset(&addr , 0 , sizeof(struct sockaddr_in));
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    addr.sin_port = htons(3000);
-    std::cout << "addr.sin_port = " << addr.sin_port << std::endl;
-    std::cout << "addr.sin_addr.s_addr = " << addr.sin_addr.s_addr << std::endl;
-    if (bind(serverSocket, (sockaddr *)&addr, sizeof(addr)) == -1) {
-        perror("bind: ");
-    }
-    if (listen(serverSocket, 0) == -1) {
-        perror("listen: ");
-    };
-    while(true) {
-        int clientSocket = 0;
-        struct sockaddr_in addrClient;
-        socklen_t len = 100;
-        memset(&addrClient , 0 , sizeof(struct sockaddr_in));
-        std::cout << "before\n";
-        // clientSocket = accept(serverSocket, (sockaddr *)&addrClient, &len);
-        clientSocket = accept(serverSocket, NULL, NULL);
-        std::cout << "clientSocket = " << clientSocket << std::endl;
-        std::cout << "after\n";
-        std::cout << "addrClient.sin_port = " << addrClient.sin_port << std::endl;
-        std::cout << "addrClient.sin_addr.s_addr = " << addrClient.sin_addr.s_addr << std::endl;
-        if (clientSocket == -1) {
-            perror("accept: ");
-        }
-        char buf[2044];
-        std::cout << "recv\n";
-        if (recv(clientSocket, buf, sizeof(buf), 0) == -1) {
-            perror("recv :");
-        }
-        std::cout << "recv after\n";
-        std::cout << buf << std::endl;
-
-        const char* reply =
-            "HTTP/1.1 200 OK\r\n"
-            "Content-Type: text/html\r\n"
-            "Content-Length: 150\r\n"
-            "Connection: keep-alive\r\n"
-            "Server : webserv\r\n"
-            "\r\n"
-            "<!DOCTYPE html><html><head>  <title>Title of the document</title></head><body>  <h1>This is a heading</h1>  <p>This is a paragraph.</p></body></html>\n";
-
-        std::cout << "send\n";
-        if (send(clientSocket, reply, strlen(reply), 0) == -1)
-        {
-            perror("send :");
-        }
-        std::cout << "send after\n";
-        close(clientSocket);
-    }
-    close(serverSocket);
+    Server server("127.0.0.1", 3000);
+    server.start();
+    // std::cout << server.get("/index.html") << std::endl;
 }
+
+
+// int main() {
+//     int a = 1;
+//     int serverSocket = socket (AF_INET, SOCK_STREAM, 0);
+//     if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &a, sizeof(int)) < 0) {
+//         std::cerr << "setsockopt(SO_REUSEADDR) failed" << std::endl;
+//     }
+//     struct sockaddr_in addr;
+//     memset(&addr , 0 , sizeof(struct sockaddr_in));
+//     addr.sin_family = AF_INET;
+//     addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+//     addr.sin_port = htons(3000);
+//     std::cout << "addr.sin_port = " << addr.sin_port << std::endl;
+//     std::cout << "addr.sin_addr.s_addr = " << addr.sin_addr.s_addr << std::endl;
+//     if (bind(serverSocket, (sockaddr *)&addr, sizeof(addr)) == -1) {
+//         perror("bind: ");
+//     }
+//     if (listen(serverSocket, 0) == -1) {
+//         perror("listen: ");
+//     };
+//     while(true) {
+//         int clientSocket = 0;
+//         struct sockaddr_in addrClient;
+//         socklen_t len = 100;
+//         memset(&addrClient , 0 , sizeof(struct sockaddr_in));
+//         std::cout << "before\n";
+//         // clientSocket = accept(serverSocket, (sockaddr *)&addrClient, &len);
+//         clientSocket = accept(serverSocket, NULL, NULL);
+//         std::cout << "clientSocket = " << clientSocket << std::endl;
+//         std::cout << "after\n";
+//         std::cout << "addrClient.sin_port = " << addrClient.sin_port << std::endl;
+//         std::cout << "addrClient.sin_addr.s_addr = " << addrClient.sin_addr.s_addr << std::endl;
+//         if (clientSocket == -1) {
+//             perror("accept: ");
+//         }
+//         char buf[2044];
+//         std::cout << "recv\n";
+//         if (recv(clientSocket, buf, sizeof(buf), 0) == -1) {
+//             perror("recv :");
+//         }
+//         std::cout << "recv after\n";
+//         std::cout << buf << std::endl;
+
+//         const char* reply =
+//             "HTTP/1.1 200 OK\r\n"
+//             "Content-Type: text/html\r\n"
+//             "Content-Length: 150\r\n"
+//             "Connection: keep-alive\r\n"
+//             "Server : webserv\r\n"
+//             "\r\n"
+//             "<!DOCTYPE html><html><head>  <title>Title of the document</title></head><body>  <h1>This is a heading</h1>  <p>This is a paragraph.</p></body></html>\n";
+
+//         std::cout << "send\n";
+//         if (send(clientSocket, reply, strlen(reply), 0) == -1)
+//         {
+//             perror("send :");
+//         }
+//         std::cout << "send after\n";
+//         close(clientSocket);
+//     }
+//     close(serverSocket);
+// }
 
 
 

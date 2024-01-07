a0745468226b5b2863a2c8d0961dbb0f63cc6b26
vahannn <tumolabs@c2r1s5.42yerevan.am>
2024-01-07 19:38:54 +0400

    py script corrected

--- www/server1/upload.py
+++ www/server1/upload.py
@@ -9,9 +9,11 @@ from cgi import FieldStorage
 SERVER_WRITE_PATH = os.environ['SERVER_WRITE_PATH']
 form = FieldStorage(sys.stdin.buffer)
 
+i = 0
 for form_file in form.list or []:
     with open(f"{SERVER_WRITE_PATH}{form_file.filename}", 'wb') as fd:
-        fd.write(form.getvalue(form_file.name))
+        fd.write(form.getvalue(form_file.name)[i])
+        i+=1
 
 print("<h1>haha!</h1>")
 
